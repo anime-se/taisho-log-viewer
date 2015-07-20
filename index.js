79b361698fcac306044ab5f7a6bb94954c5255c9
Madara Uchiha <me@madara.ninja>
2015-07-20 14:56:03 +0300

    Websocket Hello World example.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var express = require('express');
-
+var fs = require('fs');
 var app = express();
 
+var expressWs = require('express-ws')(app);
+
 app.get('/', express.static(`${__dirname}/public`));
+app.ws('/echo', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log(msg);
+        ws.send(msg);
+    })
+});
 
 app.listen(3000, function() {
     console.log('Server running!');
